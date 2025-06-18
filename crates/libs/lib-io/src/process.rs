36b8f6229043abce103e3de0e8e674f227943f4a
André <andre_miguel_costa@hotmail.com>
2025-06-18 12:13:45 +0200

    feat: implement lib io

---
+++ crates/libs/lib-io/src/process.rs
@@ -0,0 +1,88 @@
+use std::{
+    ffi::OsStr,
+    io,
+    process::{Child, Command, ExitStatus, Stdio},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+    thread::sleep,
+    time::Duration,
+};
+
+#[derive(Debug)]
+pub enum ProcessError {
+    WaitChildFail,
+    SpawnProcessFail(io::Error),
+    Quit,
+}
+pub enum ProcessStatus {
+    Done(ExitStatus),
+    Running,
+}
+/// Launches a sub process `cmd` using `args`
+/// Stdout and stderr are piped and can then be retrieved using the Child returned
+/// eg: child.stdout.take() and child.stderr.take()
+pub fn spawn_process(cmd: &str, args: Vec<String>) -> Result<Child, ProcessError> {
+    let child = Command::new(OsStr::new(&cmd))
+        .args(args)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .map_err(|err| ProcessError::SpawnProcessFail(err))?;
+
+    Ok(child)
+}
+/// Polls the process status without blocking
+/// Can be called in a loop as it will handling sleep so it doesn't use 100% of the cpu
+/// This function may never return ProcessStatus::Done if it blocks waiting for stdin
+/// In order to make sure we can actually kill a process that is blocked waiting for stdin,
+/// call `stop_child` and then `capture_exit_status`
+pub fn get_process_status(child: &mut Child) -> Result<ProcessStatus, ProcessError> {
+    match child.try_wait() {
+        Ok(status) => match status {
+            Some(exit_status) => Ok(ProcessStatus::Done(exit_status)),
+            None => {
+                sleep(Duration::from_millis(10));
+                Ok(ProcessStatus::Running)
+            }
+        },
+        Err(_) => return Err(ProcessError::WaitChildFail),
+    }
+}
+
+/// Kills Child
+pub fn stop_child(child: &mut Child) -> Result<(), io::Error> {
+    child.kill()
+}
+/// Captures the child exit status
+/// This function will wait for the child process and drop the stdin pipe so if the child process
+/// is blocked waiting for stdin, this will unblock it
+pub fn capture_exit_status(child: &mut Child) -> Result<ExitStatus, io::Error> {
+    child.wait()
+}
+
+/// Waits for the child process to end
+/// While waiting, setting `should_stop` to true will kill the process
+pub fn wait_child(
+    child: &mut Child,
+    should_stop: Arc<AtomicBool>,
+) -> Result<ExitStatus, ProcessError> {
+    loop {
+        if should_stop.load(Ordering::Relaxed) {
+            let _ = stop_child(child);
+            return Err(ProcessError::Quit);
+        }
+        match get_process_status(child) {
+            Ok(status) => match status {
+                ProcessStatus::Done(exit_status) => return Ok(exit_status),
+                ProcessStatus::Running => {
+                    sleep(Duration::from_millis(10));
+                }
+            },
+            Err(_) => {
+                return Err(ProcessError::WaitChildFail);
+            }
+        }
+    }
+}
---
+++ crates/libs/lib-io/src/runner.rs
@@ -0,0 +1,235 @@
+use std::{
+    io::{BufRead, BufReader, Read},
+    process::ExitStatus,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+        mpsc::Sender,
+    },
+    thread::{self, JoinHandle, sleep},
+    time::Duration,
+};
+
+use crate::process::{
+    ProcessStatus, capture_exit_status, get_process_status, spawn_process, stop_child,
+};
+
+#[derive(Debug, PartialEq)]
+pub enum RunEvent {
+    ProcessCreationFailed(String),
+    ProcessCreated,
+    ProcessEnd(bool),
+    ProcessNewOutputLine(String),
+}
+// A wrapper for running process and handling process events
+pub struct Runner {
+    command: String,
+    args: Vec<String>,
+}
+
+impl Runner {
+    pub fn new(command: String, args: Vec<String>) -> Self {
+        Self { command, args }
+    }
+    pub fn get_full_command(&self) -> String {
+        format!("{} {}", &self.command, &self.args.join(" "))
+    }
+
+    fn read_stream<T: Read>(tx: Sender<RunEvent>, stream: T) {
+        let reader = BufReader::new(stream);
+        reader.lines().for_each(|line| match line {
+            Ok(line) => {
+                let _ = tx.send(RunEvent::ProcessNewOutputLine(line));
+            }
+            Err(_) => return,
+        });
+    }
+    fn launch_stream_reader<T>(tx: Sender<RunEvent>, stream: T) -> JoinHandle<()>
+    where
+        T: Read + Send + 'static,
+    {
+        thread::spawn(move || Runner::read_stream(tx, stream))
+    }
+
+    // Starts the process and monitors its events
+    // It will read stdout and stderr until the child process finishes
+    // See `RunEvent` to see what events this function produces
+    pub fn run(
+        &self,
+        tx: Sender<RunEvent>,
+        should_stop: Arc<AtomicBool>,
+    ) -> Result<ExitStatus, ()> {
+        let mut process = spawn_process(&self.command, self.args.clone()).map_err(|err| {
+            let _ = tx.send(RunEvent::ProcessCreationFailed(format!("{:?}", err)));
+        })?;
+
+        let _ = tx.send(RunEvent::ProcessCreated);
+
+        // Take stdout and stderr and launch a stream reader for each
+        let mut stdout_thread = {
+            if let Some(stdout) = process.stdout.take() {
+                Some(Runner::launch_stream_reader(tx.clone(), stdout))
+            } else {
+                None
+            }
+        };
+        let mut stderr_thread = {
+            if let Some(stderr) = process.stderr.take() {
+                Some(Runner::launch_stream_reader(tx.clone(), stderr))
+            } else {
+                None
+            }
+        };
+
+        // Loop forever until we either get asked to stop or the process ends
+        let exit_status = loop {
+            if should_stop.load(Ordering::Relaxed) {
+                if stop_child(&mut process).is_err() {
+                    break None;
+                }
+                break capture_exit_status(&mut process).ok();
+            }
+            match get_process_status(&mut process) {
+                Err(_) => break None,
+                Ok(ProcessStatus::Done(status)) => break Some(status),
+                Ok(ProcessStatus::Running) => {
+                    sleep(Duration::from_millis(100));
+                }
+            };
+        };
+
+        // Join stdout and stderr threads
+        if let Some(t) = stdout_thread.take() {
+            let _ = t.join();
+        }
+
+        if let Some(t) = stderr_thread.take() {
+            let _ = t.join();
+        }
+
+        let success = if let Some(exit_status) = exit_status {
+            exit_status.success()
+        } else {
+            false
+        };
+
+        let _ = tx.send(RunEvent::ProcessEnd(success));
+        exit_status.ok_or(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        env, os,
+        process::Command,
+        sync::mpsc::{Receiver, channel},
+    };
+
+    use ntest::timeout;
+
+    use super::*;
+
+    fn compile_program(c_file: &str, target: &str) {
+        let output = Command::new("gcc")
+            .arg(c_file)
+            .arg("-o")
+            .arg(target)
+            .output()
+            .expect("Couldn't compile program");
+        println!("Output: {:?}", output);
+    }
+    fn launch_program(
+        target: &str,
+        stop: Arc<AtomicBool>,
+    ) -> (JoinHandle<Result<ExitStatus, ()>>, Receiver<RunEvent>) {
+        let runner = Runner::new(target.to_string(), vec![]);
+
+        let (tx, rx) = channel();
+        let thread_stop = stop.clone();
+
+        (
+            thread::spawn(move || {
+                let exit = runner.run(tx, thread_stop);
+                assert!(exit.is_ok());
+                exit
+            }),
+            rx,
+        )
+    }
+    fn run_blocking_program(target: &str) {
+        sleep(Duration::from_secs(1));
+        let stop = Arc::new(AtomicBool::new(false));
+        let (handler, _) = launch_program(target, stop.clone());
+        // Stop should kill the process no matter the condition it is in
+        stop.store(true, Ordering::Relaxed);
+        handler
+            .join()
+            .expect("Couldn't join thread")
+            .expect("Couldn't get child exit status");
+    }
+    fn compile_and_run_blocking_program(c_file: &str, target: &str) {
+        compile_program(c_file, target);
+        run_blocking_program(target);
+        let _ = std::fs::remove_file(target);
+    }
+    #[test]
+    #[timeout(5000)]
+    fn test_stuck_stdin() {
+        // This code blocks reading stdin forever
+        println!("{:?}", env::current_dir());
+        let c_file = "./tests/assets/wait_stdin.c";
+        let target = "./wait_stdin";
+        compile_and_run_blocking_program(c_file, target);
+    }
+
+    #[test]
+    #[timeout(5000)]
+    fn test_infinite_loop() {
+        // This code does while(1)
+        let c_file = "./tests/assets/infinite_loop.c";
+        let target = "./infinite_loop";
+        compile_and_run_blocking_program(c_file, target);
+    }
+
+    #[test]
+    #[timeout(5000)]
+    fn test_infinite_loop_with_sig_mapped() {
+        // This code enters an inifinite loop and ignores sigterm and sigint
+        let c_file = "./tests/assets/infinite_loop_map_signals.c";
+        let target = "./infinit_loop_map_signals";
+        compile_and_run_blocking_program(c_file, target);
+    }
+
+    #[test]
+    #[timeout(10000)]
+    fn test_stdout_during_run() {
+        //This code loops forever and prints Hello <i> every second
+        let c_file = "./tests/assets/infinite_loop.c";
+        let target = "./infinite_loop_stdout";
+        compile_program(c_file, target);
+
+        let stop = Arc::new(AtomicBool::new(false));
+        let (handler, rx) = launch_program(target, stop.clone());
+
+        //give the program some time to start
+        sleep(Duration::from_millis(1000));
+
+        assert_eq!(
+            rx.recv().expect("Didn't receive data from process"),
+            RunEvent::ProcessCreated
+        );
+        for i in 1..=4 {
+            assert_eq!(
+                rx.recv().expect("Didn't receive data from process"),
+                RunEvent::ProcessNewOutputLine(String::from(format!("Hello {}", i)))
+            );
+        }
+        stop.store(true, Ordering::Relaxed);
+        handler
+            .join()
+            .expect("Couldn't join thread")
+            .expect("Couldn't get child exit status");
+        let _ = std::fs::remove_file(target);
+    }
+}
