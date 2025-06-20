b04620b4dd65ab2112551c69d5d3773e130087c5
André <andre_miguel_costa@hotmail.com>
2025-06-20 15:56:14 +0200

    feat: create deployable job entity

--- crates/libs/lib-core/src/ej_job/api.rs
+++ crates/libs/lib-core/src/ej_job/api.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-use crate::{db::connection::DbConnection, prelude::*};
+use crate::{db::connection::DbConnection, ej_job::db::EjJobCreate, prelude::*};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EjJob {
     pub commit_hash: String,
@@ -8,8 +9,27 @@ pub struct EjJob {
     pub remote_token: Option<String>,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EjDeployableJob {
+    pub id: Uuid,
+    pub commit_hash: String,
+    pub remote_url: String,
+    pub remote_token: Option<String>,
+}
+
 impl EjJob {
-    pub fn create(self, connection: &mut DbConnection) -> Result<Self> {
-        Ok(self)
+    pub fn create(self, connection: &mut DbConnection) -> Result<EjDeployableJob> {
+        let job = EjJobCreate {
+            commit_hash: self.commit_hash,
+            remote_url: self.remote_url,
+        };
+        let job = job.save(connection)?;
+
+        Ok(EjDeployableJob {
+            id: job.id,
+            commit_hash: job.commit_hash,
+            remote_url: job.remote_url,
+            remote_token: self.remote_token,
+        })
     }
 }
