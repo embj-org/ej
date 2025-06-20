4d9175bb7c10a1fe2a212329add8a3fe6774b674
André <andre_miguel_costa@hotmail.com>
2025-06-20 09:20:25 +0200

    chore: remove useless if exists from migration down.sql

--- migrations/2025-05-16-144002_config/down.sql
+++ migrations/2025-05-16-144002_config/down.sql
@@ -1,6 +1,6 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS ejboard_config_tag;
-DROP TABLE IF EXISTS ejtag;
-DROP TABLE IF EXISTS ejboard_config;
-DROP TABLE IF EXISTS ejboard;
-DROP TABLE IF EXISTS ejconfig;
+DROP TABLE ejboard_config_tag;
+DROP TABLE ejtag;
+DROP TABLE ejboard_config;
+DROP TABLE ejboard;
+DROP TABLE ejconfig;
